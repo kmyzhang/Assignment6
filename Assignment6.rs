1fd3386822294556f7feab336657e9ccc2eb585d
kmyzhang <kimberly.m.zhang@gmail.com>
2019-03-05 17:12:10 -0800

    starting data types

---
+++ Assignment6.rs
@@ -0,0 +1,15 @@
+enum ExprC {
+    NumC { n: f32 },
+    IdC { s: char },
+    StringC { s: String },
+}
+
+enum Value {
+    NumV { n: f32 },
+    BoolV { b: bool },
+    StringV { s: String }, 
+}
+
+fn main() {
+
+}
