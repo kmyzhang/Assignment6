444a43a8eaed2fe02bc358653d683073bd20b8f4
kmyzhang <kimberly.m.zhang@gmail.com>
2019-03-05 18:09:35 -0800

    added top-environment

--- Assignment6.rs
+++ Assignment6.rs
@@ -2,10 +2,10 @@ use std::collections::HashMap;
 
 enum ExprC {
     NumC { n: f32 },
-    IdC { s: char },
+    IdC { s: String },
     StringC { s: String },
     IfC { cond: Box<ExprC>, thenn: Box<ExprC>, elsee: Box<ExprC> },
-    LamC { args: Vec<char>, body: Box<ExprC> },
+    LamC { args: Vec<String>, body: Box<ExprC> },
     AppC { func: Box<ExprC>, args: Vec<Box<ExprC>> },
 }
 
@@ -13,12 +13,24 @@ enum Value {
     NumV { n: f32 },
     BoolV { b: bool },
     StringV { s: String }, 
-    CloV { args: Vec<char>, body: Box<ExprC>, env: Environment },
-    PrimV { op: char },
+    CloV { args: Vec<String>, body: Box<ExprC>, env: Environment },
+    PrimV { op: String },
 }
 
-type Environment = HashMap<char, Value>;
+type Environment = HashMap<String, Value>;
+fn make_environment() -> Environment {
+    let mut env = Environment::new();
+    env.insert(String::from("+"), Value::PrimV { op: "+".to_string()});
+    env.insert(String::from("-"), Value::PrimV { op: "-".to_string()});
+    env.insert(String::from("*"), Value::PrimV { op: "*".to_string()});
+    env.insert(String::from("/"), Value::PrimV { op: "/".to_string()});
+    env.insert(String::from("<="), Value::PrimV { op: "<=".to_string()});
+    env.insert(String::from("equal?"), Value::PrimV { op: "equal?".to_string()});
+    env.insert(String::from("true"), Value::BoolV { b: true});
+    env.insert(String::from("true"), Value::BoolV { b: false});
+    return env;
+}
 
 fn main() {
-
+    let environment = make_environment();
 }
