5a4a66700d1972da89369d7034e85c45b1fea0ab
kmyzhang <kimberly.m.zhang@gmail.com>
2019-03-05 17:43:40 -0800

    added ExprC and Value types

--- Assignment6.rs
+++ Assignment6.rs
@@ -1,14 +1,23 @@
+use std::collections::HashMap;
+
 enum ExprC {
     NumC { n: f32 },
     IdC { s: char },
     StringC { s: String },
+    IfC { cond: Box<ExprC>, thenn: Box<ExprC>, elsee: Box<ExprC> },
+    LamC { args: Vec<char>, body: Box<ExprC> },
+    AppC { func: Box<ExprC>, args: Vec<Box<ExprC>> },
 }
 
 enum Value {
     NumV { n: f32 },
     BoolV { b: bool },
     StringV { s: String }, 
+    CloV { args: Vec<char>, body: Box<ExprC>, env: Environment },
+    PrimV { op: char },
 }
+
+type Environment = HashMap<char, Value>;
 
 fn main() {
 
